a721d5bf0c785c3850804c8ec5a91a58dd83cb58
Rixon <rixonraj16@gmail.com>
2023-03-25 18:24:06 +0530

    ImageProblemResolved

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -4,11 +4,10 @@ import React from 'react'
 const CategoryCard = ({imgUrl,title}) => {
   return (
    <TouchableOpacity className="relative mr-2">
-    <Image
-    source={{
-        uri: imgUrl.toString(),
-    }}
-    className="h-20 w-20 rounded" />
+    <Image 
+          source={imgUrl}
+          className="h-20 w-20 "/>
+
     <Text className="absolute bottom-1 left-1 text-white font-bold">
         {title}
     </Text>
